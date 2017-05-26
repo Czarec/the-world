c51b3308c0c5ca1dfd6c126bfbd6a493ef4d0613
Cezary Wójcik <cezary.wojcik@gft.com>
2017-05-26 15:41:40 +0200

    some startup steps

---
+++ TheWorld/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    var el = $('#username');
+    el.text('Test');
+
+    var main = $('#main');
+    main.on('mouseenter', function () {
+        main.css('background-color', '#888');
+    });
+    main.on('mouseleave', function () {
+        main.css('background-color', '');
+    });
+
+    var menuItems = $('ul.menu li a');
+
+}) ();
