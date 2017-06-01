b78b7343dca9577b37653887503c9e264a234572
Cezary Wójcik <cezary.wojcik@gft.com>
2017-06-01 14:23:13 +0200

    added bootstrap

--- TheWorld/wwwroot/js/site.js
+++ TheWorld/wwwroot/js/site.js
@@ -1,12 +1,9 @@
 ﻿(function () {
     var $sidebarAndWrapper = $('#sidebar,#wrapper');
+    var $icon = $('#sidebarToggle i.fa');
 
     $('#sidebarToggle').on("click", function () {
         $sidebarAndWrapper.toggleClass('hide-sidebar');
-        if ($sidebarAndWrapper.hasClass('hide-sidebar')) {
-            $(this).text('Show Sidebar');
-        } else {
-            $(this).text('Hide Sidebar');
-        }
+        $icon.toggleClass('fa-angle-left fa-angle-right');
     });    
 }) ();
